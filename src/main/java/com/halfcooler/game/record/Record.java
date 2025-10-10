4a2e4377e826c334f709d637ec953920bda3a170
HalfCooler <79041742+NeptuneZhao@users.noreply.github.com>
2025-10-10 08:24:29 +0800

    Change 'time' and 'difficulty' types in Record class

--- src/main/java/com/halfcooler/game/record/Record.java
+++ src/main/java/com/halfcooler/game/record/Record.java
@@ -1,4 +1,4 @@
 package com.halfcooler.game.record;
 
 /// 类型为记录的记录类
-public record Record(String userName, String time, int score, String difficulty) { }
+public record Record(String userName, int time, int score, int difficulty) { }
